838413eac508d13e253260785bda676222e52afe
Ian Hall <ianmhall6@gmail.com>
2022-06-14 15:57:21 -0400

    lab complete

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,47 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {this.currentTime++
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+  }, 1000);
   }
-
+  
   getMinutes() {
-    // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0
+    } else if (this.currentTime >= 60) { 
+      return Math.floor(this.currentTime / 60);
   }
+}
 
   getSeconds() {
-    // ... your code goes here
+     return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value > 9) { 
+return value.toString()
+  //  } else if ( value == 0) {
+  //    return '00'
+   } else return '0' + value
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    
   }
 }
 
@@ -37,3 +50,5 @@ class Chronometer {
 if (typeof module !== 'undefined') {
   module.exports = Chronometer;
 }
+
+
